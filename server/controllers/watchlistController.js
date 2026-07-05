512823aa2fd26427242542b744f29b90f73859b0
Aniket mandal <aniketmandal012@gmail.com>
2026-07-05 18:08:32 +0530

    Add delete functionality to watchlist and update routes

--- server/controllers/watchlistController.js
+++ server/controllers/watchlistController.js
@@ -1,3 +1,4 @@
+import userModel from "../models/userModel.js";
 import watchlistModel from "../models/watchlistModel.js";
 
 export const watchlist = async (req, res)=>{
@@ -21,6 +22,20 @@ export const watchlist = async (req, res)=>{
         if(error.code === 11000){
         return res.json({success: false, message: 'Already in your watchlist'})
         }
+        return res.json({success: false, message: error.message})
+    }
+}
+export const deleteWatchlistItem = async(req, res)=>{
+    const userId = req.user
+    const {mediaId} = req.body
+
+    try {
+        const deleteItem = await watchlistModel.findOneAndDelete({userId, mediaId})
+        if(!deleteItem){
+            return res.json({status: false, message: 'Item not found in watchlist'})
+        }
+        return res.json({success: true,message: 'item delete successful',  deleteItem})
+    } catch (error) {
         return res.json({success: false, message: error.message})
     }
 }
--- server/routes/watchlistRoutes.js
+++ server/routes/watchlistRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { authUser } from '../middleware/authMiddleware.js';
-import { watchlist } from '../controllers/watchlistController.js';
+import { deleteWatchlistItem, watchlist } from '../controllers/watchlistController.js';
 import { getUserWatchlist } from '../controllers/getWatchlistController.js';
 import { updateWatchlistStatus } from '../controllers/updateStautusController.js';
 
@@ -9,3 +9,5 @@ export const watchlistRoute = express.Router()
 watchlistRoute.post('/add', authUser, watchlist)
 watchlistRoute.get('/get', authUser, getUserWatchlist)
 watchlistRoute.post('/update-status', authUser, updateWatchlistStatus)
+watchlistRoute.post('/delete', authUser, deleteWatchlistItem)
+
